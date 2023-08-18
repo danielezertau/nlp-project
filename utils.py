fdd62b046f3f558d1f863a493f1f7d93e33df0ef
danielezertau <danielezer@mail.tau.ac.il>
2023-08-18 16:07:08 +0300

    Print the mean prediction

--- utils.py
+++ utils.py
@@ -573,7 +573,7 @@ class CCS(object):
             p0, p1 = self.best_probe(x0), self.best_probe(x1)
         avg_confidence = 0.5*(p0 + (1-p1))
         predictions = (avg_confidence.detach().cpu().numpy() < 0.5).astype(int)[:, 0]
-        print(f"The model's predictions are: {predictions}")
+        print(f"The model's predictions are: {predictions}.\nThe model's mean prediction is {predictions.mean()}")
         acc = (predictions == y_test).mean()
         acc = max(acc, 1 - acc)
 
